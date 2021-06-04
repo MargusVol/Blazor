bc3c10273923dcca192fdc3fef6711445e16fd46
Margus Volkov <margus.volkov@hotmail.com>
2021-06-04 17:36:47 +0300

    Part 16

--- EmployeeManagement.api/Controllers/EmployeesController.cs
+++ EmployeeManagement.api/Controllers/EmployeesController.cs
@@ -53,5 +53,26 @@ namespace EmployeeManagement.api.Controllers
                     "Error retrieving data from the database");
             }
         }
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            try
+            {
+                if(employee == null)
+                {
+                    return BadRequest();
+                }
+
+                var createdEmployee = await employeeRepository.AddEmployee(employee);
+
+                return CreatedAtAction(nameof(GetEmployee), new { id = createdEmployee.EmployeeId },
+                    createdEmployee);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error retrieving data from the database");
+            }
+        }
     }
 }
