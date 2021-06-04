6446b52b225f36f97c0de3a970aa4cdb11eb4436
Margus Volkov <margus.volkov@hotmail.com>
2021-06-04 18:18:19 +0300

    Part 19

--- EmployeeManagement.api/Models/IEmployeeRepository.cs
+++ EmployeeManagement.api/Models/IEmployeeRepository.cs
@@ -13,6 +13,6 @@ namespace EmployeeManagement.api.Models
         Task<Employee> GetEmployeeByEmail(string email);
         Task<Employee> AddEmployee(Employee employee);
         Task<Employee> UpdateEmployee(Employee employee);
-        void DeleteEmployee(int employeeId);
+        Task<Employee> DeleteEmployee(int employeeId);
     }
 }
